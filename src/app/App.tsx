322515e0c60b9873cbff638055a8fa3ef8072ca2
apexlinks <sales.apexlinks@gmail.com>
2026-02-03 05:46:13 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,25 @@
 import { Navigation } from '@/app/components/Navigation';
 import { Hero } from '@/app/components/Hero';
 import { InfluencerShowcase } from '@/app/components/InfluencerShowcase';
+// 1. Import the Partners component
+import Partners from '@/app/components/Partners'; 
 import { Services } from '@/app/components/Services';
 import { Footer } from '@/app/components/Footer';
 import { LanguageProvider } from '@/app/context/LanguageContext';
-// 1. IMPORT THE NEW MODAL PROVIDER
 import { ContactModalProvider } from '@/app/context/ContactModalContext';
 
 export default function App() {
   return (
     <LanguageProvider>
-      {/* 2. WRAP THE APP WITH IT */}
       <ContactModalProvider>
         <div className="size-full bg-black text-white overflow-x-hidden">
           <Navigation />
           <Hero />
           <InfluencerShowcase />
+          
+          {/* 2. Add the component here */}
+          <Partners /> 
+          
           <Services />
           <Footer />
         </div>
